4030c9e68aaf193c9df683ff584ff4c5d2d7d1d5
iraklis <iraklis.bekiaris@kebormed.com>
2022-11-25 16:27:03 +0200

    Insert actions button templates to users-list

--- src/app/shared/components/user-list/user-list.component.ts
+++ src/app/shared/components/user-list/user-list.component.ts
@@ -1,15 +1,21 @@
-import {Component, Input} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {MatTableModule} from "@angular/material/table";
+import { Component, Input, TemplateRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from "@angular/material/table";
+import { ConfirmActionDirective } from '../UI/confirm-action/confirm-action.directive';
 
 @Component({
   selector: 'kbm-user-list',
   standalone: true,
-  imports: [CommonModule, MatTableModule],
+  imports: [CommonModule, MatTableModule, ConfirmActionDirective],
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent {
   @Input() data: any[] = [];
-  displayedColumns = ['id', 'name', 'username', 'email']
+  @Input() btns: TemplateRef<any>[] = []
+  displayedColumns = ['id', 'name', 'username', 'email', 'btns'];
+
+  delete(event: any) {
+
+  }
 }
